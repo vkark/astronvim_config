4cbaba618aa8991c174ebb8815d5901a018de40e
Vesa Kärkkäinen <17165261+vkark@users.noreply.github.com>
2023-05-17 23:46:15 +0300

    add default language parsers for treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "awk",
+      "bash",
+      "c",
+      "cmake",
+      "cpp",
+      "julia",
+      "kdl",
+      "latex",
+      "lua",
+      "matlab",
+      "meson",
+      "python",
+      "rust",
+      "sql",
+      "toml",
+      "yaml",
     })
   end,
 }
